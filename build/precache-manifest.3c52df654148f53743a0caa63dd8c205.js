64014756dabf2def96c738f093afe961dd528f1b
ahmed <a.elbehary@berlin-direktversicherung.de>
2019-02-08 16:08:56 +0100

    second commit

---
+++ build/precache-manifest.3c52df654148f53743a0caa63dd8c205.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "71af173bcfc2deb246b3",
+    "url": "/static/js/main.71af173b.chunk.js"
+  },
+  {
+    "revision": "8fb9402ce43b008f76b3",
+    "url": "/static/js/1.8fb9402c.chunk.js"
+  },
+  {
+    "revision": "71af173bcfc2deb246b3",
+    "url": "/static/css/main.4b0c14f9.chunk.css"
+  },
+  {
+    "revision": "cca85f90614188e7f1dc65084cd0c2c3",
+    "url": "/index.html"
+  }
+];
