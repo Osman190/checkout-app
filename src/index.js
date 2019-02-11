0d2419a928da41855e2903ba56c1b658b02a066c
ahmed <a.elbehary@berlin-direktversicherung.de>
2019-02-11 10:01:04 +0100

    WIP

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-export { App as Checkout };
+export default Checkout => {
+  return App;
+};
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 
