7fad6a5a1b2ebcbcddf75fb87eb3eab710b3841d
ahmed <a.elbehary@berlin-direktversicherung.de>
2019-02-11 10:09:07 +0100

    WIP

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 
-export class App
+export default class {
+  App;
+}
 
 // ReactDOM.render(<App />, document.getElementById("root"));
 
-// serviceWorker.unregister();
+serviceWorker.unregister();
